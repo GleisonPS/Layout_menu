bda45541303bdebe131678248c80409c064d5bc9
Gleison_Pereira <87569717+GleisonPS@users.noreply.github.com>
2022-10-17 22:09:23 -0300

    add script JavaScript e do CSS

---
+++ botao.js
@@ -0,0 +1,21 @@
+
+        /* Quando o usuário clica no botão,
+        alternar entre ocultar e mostrar o conteúdo suspenso */
+        function myFunction() {
+            document.getElementById("myDropdown").classList.toggle("show");
+            
+            }
+    
+            // Feche o menu suspenso se o usuário clicar fora dele
+            window.onclick = function(event) {
+            if (!event.target.matches('.dropbtn')) {
+                var dropdowns = document.getElementsByClassName("dropdown-content");
+                var i;
+                for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+                }
+            }
+    }
